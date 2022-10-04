31bf30767852c71c4d835c0ea9f3818e33cd58ba
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2022-10-04 12:55:04 +0530

    testing git command

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,8 @@ const app = express()
 const port = 3000
 
 //Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 
 app.listen(port, () => {
